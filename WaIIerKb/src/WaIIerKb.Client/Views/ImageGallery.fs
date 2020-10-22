817f36444a15ff748cf990dc959a75d00c861747
bwall72 <brandonw@live.com>
2020-10-22 00:16:40 -0400

    add image gallery

---
+++ WaIIerKb/src/WaIIerKb.Client/Views/ImageGallery.fs
@@ -0,0 +1,51 @@
+module WaIIerKb.Client.Views.ImageGallery
+
+open System
+open Bolero.Html
+
+[<Measure>]
+type px
+
+type GalleryImage =
+    { Source: string
+      Caption: string
+      AltText: string }
+
+let private MAX_WIDTH = 400<px>
+let private MAX_HEIGHT = 300<px>
+
+let private _generateRow images =
+    div
+        [ attr.``class`` "row" ]
+        (images
+         |> Array.map (fun galleryImage ->
+             div [ attr.``class``
+                       ("thumbnail col-md-"
+                        + (12 / images.Length).ToString()) ] [
+                 a [ attr.href galleryImage.Source ] [
+                     img [ attr.``class`` "img-responsive"
+                           attr.src galleryImage.Source
+                           attr.alt galleryImage.AltText
+                           attr.style
+                               ("max-height: "
+                                + (MAX_HEIGHT.ToString())
+                                + "; max-width: "
+                                + (MAX_WIDTH.ToString())) ]
+                     div [ attr.``class`` "caption" ] [
+                         text galleryImage.Caption
+                     ]
+                 ]
+             ])
+         |> Array.toList)
+
+let private _generateGalleryHtml size (images: GalleryImage list) =
+    images
+    |> List.toSeq
+    |> Seq.chunkBySize size
+    |> Seq.map (fun rowImages -> _generateRow rowImages)
+    |> Seq.toList
+
+
+
+let imageGallery (images: GalleryImage list) =
+    div [] (_generateGalleryHtml (Math.Min(images.Length, 2)) images)
